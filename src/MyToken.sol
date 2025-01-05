b8ba3fd27170bfbdc75368878e3d1b9088a21961
Kelechukwu <paparazzip8@gmail.com>
2025-01-05 16:18:35 +0100

    Created and deployed my new ERC20 token on anvil chain

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken {
-    mapping (address => uint) private s_balances;
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
 
-    function name() public pure returns (string memory){
-        return "My Token";
     }
 }
